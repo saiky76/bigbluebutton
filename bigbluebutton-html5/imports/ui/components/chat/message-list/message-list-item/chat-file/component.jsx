937105c5800ab238d665da9c6eae085d4e5f533d
sravani <sravani@bbb.pipitacademy.com>
2020-06-08 23:52:40 +0530

    chat name and  file display trail

--- bigbluebutton-html5/imports/ui/components/chat/message-list/message-list-item/chat-file/component.jsx
+++ bigbluebutton-html5/imports/ui/components/chat/message-list/message-list-item/chat-file/component.jsx
@@ -5,7 +5,11 @@ import styles from './styles';
 import Button from '../../../../button/component';
 import Icon from '/imports/ui/components/icon/component';
 import Auth from '/imports/ui/services/auth';
-
+// import { Document, Page, pdfjs ,Outline} from "react-pdf";
+//import 'react-pdf/dist/Page/AnnotationLayer.css';
+import { Document, Page, pdfjs ,Outline } from 'react-pdf/dist/entry.parcel';
+// import { Document, Page, pdfjs, Outline  } from 'react-pdf/dist/entry.webpack';
+// import pdfjs from "pdfjs-dist/webpack.js";
 const propTypes = {
   text: PropTypes.string,
   file: PropTypes.object.isRequired,
@@ -28,6 +32,9 @@ export default class ChatFileUploaded extends PureComponent {
     
     window.open(uri);
   }
+  componentDidMount() {
+    pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
+  }
   render() {
     const {
       text,
@@ -36,14 +43,30 @@ export default class ChatFileUploaded extends PureComponent {
     } = this.props;
 
     const uri = `https://${window.document.location.hostname}/bigbluebutton/file/download/`
-      + `${file.fileId}/${file.fileName}/${file.meetingId}`;
+      + `${file.fileId}/${file.fileName}/${file.meetingId}/`;
 
-    // const ext = file.fileName.split('.').pop();
+     const ext = file.fileName.split('.').pop();
     return (
       <div className={(id == Auth.userID) ? styles.senderFileWrapper : styles.fileWrapper}>
         <div className={styles.wrapper}>
            <div className={styles.extensionBox}>
-            <img src={uri} alt="" />
+              {ext=="png"  ? <img src={uri} alt="" /> :
+           
+           <Document className={styles.extension}  file = {{url: uri, pdf:uri, withCredentials:true}}  inputRef={uri} 
+           onSourceError={(error) => alert('Error while retrieving document source! ' + error.message)}
+           error={(error) => alert('Error while retrieving document source! ' + error.message)}
+           onSourceError={(error) => alert('Error while retrieving document source! ' + error.message)}
+           >
+             <Page pageNumber={1} file = {{url: uri}} pdf={uri} />
+             <Outline file = {{url: uri}} pdf={uri}/>
+           </Document>
+                //  <object data={uri}  width="40" height="40" >
+                //  {/* <a href={uri}>test.pdf</a> */}
+                //  </object>
+           }
+          
+            {/* <embed src={uri}  /> */}
+            {/* <iframe src={uri} ></iframe> */}
           </div>          
           <span className={styles.fileName}>{file.fileName}</span>
           <Button
